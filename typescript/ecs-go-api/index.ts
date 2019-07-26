117382ded4e49c5aa192dd2a865c95c28dc28212
Cagdas Ozbey <ozbeydev@hotmail.com>
2019-07-26 23:38:39 +0300

    Remove unused resources

--- typescript/ecs-go-api/index.ts
+++ typescript/ecs-go-api/index.ts
@@ -1,5 +1,4 @@
 import cdk = require("@aws-cdk/core");
-import ecr = require("@aws-cdk/aws-ecr");
 import ec2 = require("@aws-cdk/aws-ec2");
 import ecs = require('@aws-cdk/aws-ecs');
 import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
@@ -8,22 +7,16 @@ export default class EcsGoAPIStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps){
         super(scope, id, props);
 
-        const ecrRepo = new ecr.Repository(this, "GoAPI", {
-            repositoryName: "go-api"
-        });
-
-        new cdk.CfnOutput(this, "ecrRepoURI", {
-            value: ecrRepo.repositoryUri
-        });
-
         const vpc = new ec2.Vpc(this, 'vpc', { maxAzs: 2 });
 
         const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+
         const fargateService = new ecs_patterns.LoadBalancedFargateService(this, "FargateService", {
             cluster,
-            image: new ecs.ContainerImage.fromAsset("api"),
+            image: ecs.ContainerImage.fromAsset(`${__dirname}/api`),
             containerPort: 8080
         });
+
         new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: fargateService.loadBalancer.loadBalancerDnsName });
     }
 }
