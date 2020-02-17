f82628c13e5b2b88c809395d2e72f2626c14201f
Cagdas Ozbey <ozbeydev@hotmail.com>
2020-02-17 01:45:19 +0100

    Update ec2 examples

--- ec2-basics/index.ts
+++ ec2-basics/index.ts
@@ -5,10 +5,8 @@ export class EC2BasicsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Using default vpc
-    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
-      isDefault: true
-    });
+    // Create new VPC
+    const vpc = new ec2.Vpc(this, 'VPC');
 
     // Open port 22 for SSH connection from anywhere
     const mySecurityGroup = new ec2.SecurityGroup(this, 'SecurityGroup', {
@@ -22,24 +20,13 @@ export class EC2BasicsStack extends cdk.Stack {
     // We are using the latest AMAZON LINUX AMI
     const awsAMI = new ec2.AmazonLinuxImage({generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2});
 
-
     // We define instance details here
-    const ec2Instance = new ec2.CfnInstance(this, "test-instance", {
-      imageId: awsAMI.getImage(this).imageId,
-      instanceType: "t2.micro",
-      monitoring: false,
-      tags: [
-        {"key": "Name", "value": "test-instance"}
-      ],
-      networkInterfaces: [
-        {
-          deviceIndex: "0",
-          associatePublicIpAddress: true,
-          subnetId: vpc.publicSubnets[0].subnetId,
-          groupSet: [mySecurityGroup.securityGroupId]
-        }
-      ]
-    })
+    const ec2Instance = new ec2.Instance(this, 'Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.NANO),
+      machineImage: awsAMI,
+      securityGroup: mySecurityGroup
+    });
   }
 }
 
